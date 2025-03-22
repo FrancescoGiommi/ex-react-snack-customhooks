2e67ec89a8ef96da3110a245814a9a140a94d1c8
Francesco <you@example.com>
2025-03-22 17:07:24 +0100

    Add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useSwitch from "./components/useSwitch";
+import useDate from "./components/useDate";
 import "./App.css";
 
 //! Snack 1: useSwitch() – Toggle Booleano
@@ -12,19 +13,40 @@ Cosa deve fare?
     Ritorna il valore attuale e una funzione toggle() per cambiarlo.
     Quando toggle() viene chiamato, il valore passa da true → false e viceversa.
 
-Esempio di utilizzo:
+
+*/
+
+//! Snack 2: useDate() – Ottenere la Data Attuale
+
+/* Creare un custom hook che restituisca la data e l'ora correnti,
+ aggiornandosi automaticamente ogni secondo.
+
+Cosa deve fare?
+
+    Restituisce un oggetto con data e ora formattata.
+    Si aggiorna automaticamente ogni secondo.
+    Usa useEffect() per gestire l’aggiornamento.
+
 */
 
 function App() {
   const [isOn, toggle] = useSwitch(false);
+  const currentDate = useDate();
 
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+    <>
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
 
-      {/* Bottone per cambiare il valore */}
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+        {/* Bottone per cambiare il valore */}
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
+    </>
   );
 }
 
